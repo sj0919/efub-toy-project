a57b80d429b960945766b19e0093f34459b19ec6
sj0919 <161692586+sj0919@users.noreply.github.com>
2024-05-30 23:37:59 +0900

    AskRemoveModal.js
    Delete file

--- toyproject/src/components/AskRemoveModal.js
+++ toyproject/src/components/AskRemoveModal.js
@@ -1,15 +1,1 @@
-import AskModal from "./AskModal";
 
-const AskRemoveModal = ({ visible, onConfirm, onCancel }) => {
-  return (
-    <AskModal
-      visible={visible}
-      title="Delete Post"
-      description="포스트를 정말 삭제하시겠습니까?"
-      confirmText="삭제"
-      onConfirm={onConfirm}
-      onCancel={onCancel}
-    ></AskModal>
-  );
-};
-export default AskRemoveModal;
